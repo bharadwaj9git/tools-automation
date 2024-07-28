d714a73c2198647c1ec64243e6c8672a1aff1b1d
MYPC <bharadwajsai567@gmail.com>
2024-07-28 20:30:32 +0530

    spot instance

--- module/main.tf
+++ module/main.tf
@@ -31,6 +31,13 @@ resource "aws_instance" "main" {
   instance_type = var.instance_type
   vpc_security_group_ids = [aws_security_group.main.id]
 
+  instance_market_options {
+    spot_options {
+      instance_interruption_behaviour = "stop"
+      spot_instance_type = "persistent"
+    }
+  }
+
   tags = {
     Name = var.name
   }
